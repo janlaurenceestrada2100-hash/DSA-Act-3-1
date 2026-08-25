0e99133f2084cc28a08ecf69f138f12c576a0bdc
JanLaurenceEstrada <JanLaurenceEstrada@SMU-0465>
2026-08-25 09:59:41 +0800

    Add project files.

---
+++ DSA Act 3 1/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using System.Globalization;
+
+namespace DSA_Act_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * DSA Activity 2.3 - [Estrada, Jan Laurence]
+             * Program Description:
+             * 
+             */
+
+            try
+            {
+
+
+
+                int[] score = new int[10];
+                for (int x = 1; x <= 12; x++)
+                {
+                    score[x-1]=Convert.ToInt32(Console.ReadLine());
+                }
+
+                foreach (string month in score)
+                {
+                    Console.WriteLine("Scores: " + $"-> {score}");
+                }
+                Console.ReadKey();
+
+
+
+
+
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("err enctrd. " + e.ToString());
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
